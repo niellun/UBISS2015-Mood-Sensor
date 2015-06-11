e645d5a02767064c46def7bee0ea6000e99e7644
Alexander Samodelkin <niellune-sub@yandex.ru>
2015-06-12 00:40:23 +0300

    Rework

--- app/src/main/java/com/psicode/aware_mood_sensor/MainActivity.java
+++ app/src/main/java/com/psicode/aware_mood_sensor/MainActivity.java
@@ -1,15 +1,17 @@
 package com.psicode.aware_mood_sensor;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
@@ -24,130 +26,85 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends ActionBarActivity {
 
-    private SensorBroadcastReceiver mBroadcastReceiver;
-    private GlobalAccel mGlobalAceelerometer;
-    private static final ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
-    private static Button switcher_on;
+    private final SensorBroadcastReceiver _broadcastReceiver;
+    private final ScheduledExecutorService _scheduler;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private GlobalAccel _globalAccelerometer;
+    private Button _btnSwithOn;
+    private Integer _counter;
 
-        // Turn sensors on
-        mGlobalAceelerometer = new GlobalAccel(getApplicationContext());
-        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_LINEAR_ACCELEROMETER, true);
-        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_GYROSCOPE, true);
-        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_ROTATION, true);
-        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_GRAVITY, true);
+    public MainActivity() {
+        _scheduler = Executors.newSingleThreadScheduledExecutor();
+        _counter = 0;
 
-        mBroadcastReceiver = new SensorBroadcastReceiver();
+        // Set broadcast receiver
+        _broadcastReceiver = new SensorBroadcastReceiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction(LinearAccelerometer.ACTION_AWARE_LINEAR_ACCELEROMETER);
         filter.addAction(Rotation.ACTION_AWARE_ROTATION);
         filter.addAction(Gravity.ACTION_AWARE_GRAVITY);
         filter.addAction(GlobalAccel.ACTION_NEW_DATA);
-        registerReceiver(mBroadcastReceiver, filter);
+        registerReceiver(_broadcastReceiver, filter);
+    }
 
-        sendBroadcast(new Intent(Aware.ACTION_AWARE_REFRESH));
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        _btnSwithOn = (Button) findViewById(R.id.button);
 
-
-
-
-
-
-        switcher_on = (Button) findViewById(R.id.button);
-
-        switcher_on.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                // TURN LABLE ON
-
-                Log.d("BTN", "ON");
-
-                switcher_on.setText("THROW AWAY");
-                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
-                mp.start();
-                Toast.makeText(getApplicationContext(), "Throw it in 3 seconds!", Toast.LENGTH_SHORT).show();
-
-                Intent Acc_fall = new Intent(LinearAccelerometer.ACTION_AWARE_LINEAR_LABEL);
-                Acc_fall.putExtra(LinearAccelerometer.EXTRA_LABEL, "FALL");
-                getApplicationContext().sendBroadcast(Acc_fall);
-
-                Intent gyo_fall = new Intent(Gyroscope.ACTION_AWARE_GYROSCOPE_LABEL);
-                gyo_fall.putExtra(Gyroscope.EXTRA_LABEL, "FALL");
-                getApplicationContext().sendBroadcast(gyo_fall);
-
-                Intent rota_fall = new Intent(Rotation.ACTION_AWARE_ROTATION_LABEL);
-                rota_fall.putExtra(Rotation.EXTRA_LABEL, "FALL");
-                getApplicationContext().sendBroadcast(rota_fall);
-
-                Intent gra_fall = new Intent(Gravity.ACTION_AWARE_GRAVITY_LABEL);
-                gra_fall.putExtra(Gravity.EXTRA_LABEL, "FALL");
-                getApplicationContext().sendBroadcast(gra_fall);
-
-                Runnable task = new Runnable() {
-
-                    public void run() {
-                        // TURN LABLE OFF
-
-                        Log.d("BTN", "OFF");
-
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-                                // Update UI elements
-                                switcher_on.setText("TURN IT ON");
-                            }
-                        });
-
-                        Intent Acc_fall = new Intent(LinearAccelerometer.ACTION_AWARE_LINEAR_LABEL);
-                        Acc_fall.putExtra(LinearAccelerometer.EXTRA_LABEL, "");
-                        getApplicationContext().sendBroadcast(Acc_fall);
-
-                        Intent gyo_fall = new Intent(Gyroscope.ACTION_AWARE_GYROSCOPE_LABEL);
-                        gyo_fall.putExtra(Gyroscope.EXTRA_LABEL, "");
-                        getApplicationContext().sendBroadcast(gyo_fall);
-
-                        Intent rota_fall = new Intent(Rotation.ACTION_AWARE_ROTATION_LABEL);
-                        rota_fall.putExtra(Rotation.EXTRA_LABEL, "");
-                        getApplicationContext().sendBroadcast(rota_fall);
-
-                        Intent gra_fall = new Intent(Gravity.ACTION_AWARE_GRAVITY_LABEL);
-                        gra_fall.putExtra(Gravity.EXTRA_LABEL, "");
-                        getApplicationContext().sendBroadcast(gra_fall);
-
-
-
-                    }
-                };
-
-                worker.schedule(task, 5, TimeUnit.SECONDS);
-
-                /*
-                // Perform action on click
-                Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_LINEAR_ACCELEROMETER, true);
-                Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_GYROSCOPE, true);
-                Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_ROTATION, true);
-                Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_GRAVITY, true);
-
-                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
-                mp.start();
-                Toast.makeText(getApplicationContext(), "Sensors On!", Toast.LENGTH_SHORT).show();
-
-                sendBroadcast(new Intent(Aware.ACTION_AWARE_REFRESH));
-                */
-            }
-        });
-
-        gaccel = new GlobalAccel(getApplicationContext());
+        // Turn sensors on
+        _globalAccelerometer = new GlobalAccel(getApplicationContext());
         Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_LINEAR_ACCELEROMETER, true);
         Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_GYROSCOPE, true);
         Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_ROTATION, true);
         Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_GRAVITY, true);
 
+        // Update aware
+        sendBroadcast(new Intent(Aware.ACTION_AWARE_REFRESH));
     }
+
+    private void SetSensorLabels(String label)
+    {
+        Context c = getApplicationContext();
+
+        c.sendBroadcast(new Intent(LinearAccelerometer.ACTION_AWARE_LINEAR_LABEL)
+                .putExtra(LinearAccelerometer.EXTRA_LABEL, label));
+
+        c.sendBroadcast(new Intent(Gyroscope.ACTION_AWARE_GYROSCOPE_LABEL)
+                .putExtra(Gyroscope.EXTRA_LABEL, label));
+
+        c.sendBroadcast(new Intent(Rotation.ACTION_AWARE_ROTATION_LABEL)
+                .putExtra(Rotation.EXTRA_LABEL, label));
+
+        c.sendBroadcast(new Intent(Gravity.ACTION_AWARE_GRAVITY_LABEL)
+                .putExtra(Gravity.EXTRA_LABEL, label));
+    }
+
+    public void OnStartClick(View view) {
+        _btnSwithOn.setText("THROW AWAY");
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
+        mp.start();
+
+        SetSensorLabels(_counter.toString());
+
+        _scheduler.schedule(OnStop, 5, TimeUnit.SECONDS);
+    }
+
+    Runnable OnStop = new Runnable() {
+
+        public void run() {
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    // Update UI elements
+                    _btnSwithOn.setText("TURN IT ON");
+                }
+            });
+            SetSensorLabels("");
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -161,8 +118,8 @@ public class MainActivity extends ActionBarActivity {
         super.onDestroy();
         //       Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_ACCELEROMETER, false);
         sendBroadcast(new Intent(Aware.ACTION_AWARE_REFRESH));
-        unregisterReceiver(mBroadcastReceiver);
-        mGlobalAceelerometer.Destroy();
+        unregisterReceiver(_broadcastReceiver);
+        _globalAccelerometer.Destroy();
     }
 
     @Override
@@ -179,4 +136,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
